58294d108a00fd1f8524312709212c0fa6d5c470
giovannim1166 <giovannim1166@hstat.org>
2016-10-13 02:57:51 +0000

    finish

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(one,two)
+    one/two
 end
 
-def argue
-  
+def argue(three)
+  three
 end
 
-def greeting
+def greeting(greet,name)
 
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(topping="cheese")
+    topping
 end
 
-def whisper
-    
+def whisper(phrase)
+   phrase.downcase
 end
